88ceba48c5615a1caabc44a3a9056e5c702bc03c
DicksonWachai <wachaidickson@gmail.com>
2024-07-03 06:43:55 -0700

    Correct checker error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
 # installing flask using pip3 in puppet
+package { 'python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
