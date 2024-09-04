11449f5e3455cff4bc313deaf6e52df55936a96b
DicksonWachai <wachaidickson@gmail.com>
2024-09-03 23:29:55 -0700

    Correct checker error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
 # Increases the amount of traffic an Nginx server can handle.
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
+exec { 'nginx-conf':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
